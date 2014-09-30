7ae15629e638d18cd4daea45310ad9df3956802b
Jason Sohl <jason@frozensoliddesigns.com>
2014-09-29 20:09:03 -0700

    add points

--- help.md
+++ help.md
@@ -1,9 +1,3 @@
-#how to send to slack
-token=njN6d03HdOX43AWtYvs1mJOS
-team_id=T0001
-channel_id=C2147483705
-channel_name=test
-user_id=U2147483697
-user_name=Steve
-command=/weather
-text=94070
+curl -H "Content-Type: application/json" -d '{ "token": "njN6d03HdOX43AWtYvs1mJOS", "team_id": "T02B54W3K", "channel_id": "C02CD09GJ", "channel_name": "finlit", "user_id": "U02GLL9JC", "user_name": "jasonsohl", "command": "/addpoints", "text": "Jason 10" }' http://localhost:6001/
+
+//https://dopamine.slack.com/services/hooks/incoming-webhook?token=REQtnGjBsWotN3hTxCDtbzmV
--- slack.js
+++ slack.js
@@ -1,7 +1,10 @@
 var express = require('express'),
     pg      = require('pg'),
     bodyParser = require('body-parser'),
+    http = require('http')
     app = express();
+
+var conString = "postgres://slack:slackPoints@localhost/slack";
 
 app.use(bodyParser.json());
 
@@ -11,14 +14,12 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res){
 
-  var conString = "postgres://slack:slackPoints@localhost/slack";
-
   var client = new pg.Client(conString);
   client.connect(function(err) {
     if(err) {
       return console.error('could not connect to postgres', err);
     }
-    client.query('SELECT * FROM users', function(err, result) {
+    client.query('SELECT * FROM users ORDER BY points DESC', function(err, result) {
       if(err) {
         return console.error('error running query', err);
       }
@@ -33,8 +34,78 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
+  var command = req.body.text;
+  var name = command.split(" ")[0].toLowerCase();
+  var points = Math.floor(command.split(" ")[1]);
+
+  var queryString = 'SELECT points FROM users WHERE LOWER(name)=LOWER(\''+name+'\')';
+
+  var client = new pg.Client(conString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query(queryString, function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      
+      var currentPoints = result.rows[0].points;
+
+      if (typeof points === 'undefined') {
+
+        res.json({"success": false});
+
+      } else {
+
+        var newPoints = currentPoints + points;
+
+        client.query('UPDATE users SET points = $1 where LOWER(name) = LOWER($2)',[newPoints,name], function(err, result) {
+          if(err) {
+            return console.error('error running query', err);
+          }
+
+          client.end();
+
+          var data = {
+            "text": "Success! "+points+" points added to "+name+"\nThat's "+newPoints+" total!"
+          }
+          var dataString = JSON.stringify(data);
+          var dataLength = dataString.length;
+
+          var headers = {
+            'Content-Type': 'application/json',
+            'Content-Length': dataLength
+          };
+
+          var options = {
+            host: 'https://dopamine.slack.com',
+            port: 80,
+            path: '/services/hooks/incoming-webhook?token=REQtnGjBsWotN3hTxCDtbzmV',
+            method: 'POST',
+            headers: headers
+          };
+
+          var post_req = http.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+          });
+          post_req.write(dataString);
+          post_req.end();
+
+          res.json({"success":true});
+
+        });
+      }
+
+    });
+  });
+});
+
+app.post('/testing', function(req, res){
   console.log(req.body);
-  res.json({'success':true});
 });
 
 var port = process.env.PORT || 6001;
